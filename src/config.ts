9d812bf6785396251c4646a840a2bed69281365d
Waynesway <Waynesway@users.noreply.github.com>
2025-09-04 14:33:49 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,18 @@
 export default {
   // 网站标题
-  Title: '韩小韩博客',
+  Title: '无聊的博客',
   // 网站地址
-  Site: 'https://www.vvhan.com',
+  Site: 'https://blog.15062715.xyz',
   // 网站副标题
-  Subtitle: '不曾与你分享的时间,我在进步.',
+  Subtitle: '无聊以至于折腾各种没用的垃圾.',
   // 网站描述
-  Description: '韩小韩博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
+  Description: '只是不知道该记录下什么东西总是在做博客的路上。希望这次的坚持时间能够长一点了。',
   // 网站作者
-  Author: '.𝙃𝙖𝙣',
+  Author: 'Wayne',
   // 作者头像
-  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640',
+  Avatar: 'https://raw.githubusercontent.com/diegosway2023/picgopics/master/image_171.png',
   // 网站座右铭
-  Motto: '运气是计划之外的东西.',
+  Motto: '机会是给有准备的人准备的.',
   // Cover 网站缩略图
   Cover: '/assets/images/banner/072c12ec85d2d3b5.webp',
   // 网站侧边栏公告 (不填写即不开启)
@@ -65,7 +65,7 @@ export default {
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
     { text: 'Github', link: 'https://github.com/uxiaohan', icon: 'WebSite_github' },
-    { text: '韩小韩API', link: 'https://api.vvhan.com', icon: 'WebSite_api' },
+    { text: 'API', link: 'https://api.vvhan.com', icon: 'WebSite_api' },
     { text: '每日热榜', link: 'https://hot.vvhan.com', icon: 'WebSite_hot' },
     { text: '骤雨重山图床', link: 'https://wp-cdn.4ce.cn', icon: 'WebSite_img' },
     { text: 'HanAnalytics', link: 'https://analytics.vvhan.com', icon: 'WebSite_analytics' },
